2f1dfa32eef13d43b3ae12a95e2ebb162d4f6db4
ShiyxProjectCode <syxtest01@gmail.com>
2018-06-27 08:43:42 +0800

    修改appinfo命名空间

--- SkyWalking.Sample.Backend/Models/AppInfo.cs
+++ SkyWalking.Sample.Backend/Models/AppInfo.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SkyWalking.Sample.Frontend.Models
+namespace SkyWalking.Sample.Backend.Models
 {
     public class AppInfo
     {
